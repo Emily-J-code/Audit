2f7a5d53165aa34260ca7873593221079ea21d98
Emily-J-code <222336884+Emily-J-code@users.noreply.github.com>
2025-08-09 20:14:31 -0700

    added functionality to more forms

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const submitButton = document.querySelector(".submitButton");
 var allergen = "";
+var product = "";
+var business = "";
+var date = "";
 var auditCount = 1;
 
 // adding function so that a new audit will appear when button is clicked
@@ -13,5 +16,29 @@ function finishAudit(){
     document.getElementById("allergenReplacement").innerHTML = allergen;
     document.getElementById("allergenReplacement").style.visibility = "visible";
 
+    //doing same thing with the product input and text
+    product = document.getElementById("productInput").value;
+    console.log(product);
+    document.getElementById("productInput").style.visibility = "hidden";
+    document.getElementById("productReplacement").innerHTML = product;
+    document.getElementById("productReplacement").style.visibility = "visible";
+
+    //"" business
+    business = document.getElementById("businessInput").value;
+    console.log(business);
+    document.getElementById("businessInput").style.visibility = "hidden";
+    document.getElementById("businessReplacement").innerHTML = product;
+    document.getElementById("businessReplacement").style.visibility = "visible";
+
+    //"" data
+    date = document.getElementById("dateInput").value;
+    console.log(date);
+    document.getElementById("dateInput").style.visibility = "hidden";
+    document.getElementById("dateReplacement").innerHTML = date;
+    document.getElementById("dateReplacement").style.visibility = "visible";
+    
+
+
+
    
-}``
+}
