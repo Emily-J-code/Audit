7f7b4b3e2303698503640e663a6713625ce7cecf
Emily-J-code <222336884+Emily-J-code@users.noreply.github.com>
2025-08-09 18:26:15 -0700

    hiding/showing elements

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const newAuditPress = document.querySelector(".newAuditButton:");
-
-
-let auditCount = 1;
+const submitButton = document.querySelector(".submitButton");
+var allergen = "";
+var auditCount = 1;
 
 // adding function so that a new audit will appear when button is clicked
-newAuditPress.addEventListener("click", addNewAudit);
-function addNewAudit(){
+submitButton.addEventListener("click", finishAudit);
+function finishAudit(){
     auditCount++;
-    
-}
+    //getting allergen input and replacing it with the new value
+    allergen = document.getElementById("allergen").value;
+    document.getElementById("allergen").style.visibility = "hidden";
+    document.getElementById("allergenReplacement").style.visibility = "visible";
+    document.getElementById("allergenReplacement").value = allergen;
+
+    console.log(allergen);
+}``
